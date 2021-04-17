72accf5ea821d263b8e516360fe7b5e43bec8e45
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-04-17 22:32:18 +0700

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import { useState } from "react"
+let id = 0
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [todos, setTodos] = useState([])
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    let addedTodo = { value: inputValue, id: id++, isDelete: false }
+    console.log(addedTodo)
+    let newTodos = todos.concat(addedTodo)
+    setTodos(newTodos)
+    setInputValue("")
+  }
+  const hadleRemoveTodo = (id) => {
+    let filterTodo = todos.filter(todo => {
+      if (todo.id === id) {
+        todo.isDelete = !todo.isDelete
+      }
+      return todo
+    })
+    setTodos(filterTodo)
+  }
   return (
     <div className="App">
-    </div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text" name="" id="" value={inputValue} onChange={handleInputChange} />
+      </form>
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id} onClick={() => {
+              hadleRemoveTodo(todo.id)
+            }}
+              style={{ textDecoration: `${todo.isDelete === false ? "none" : "line-through"}` }}
+            >
+              {todo.value}
+            </li>
+          )
+        })}
+      </ul>
+    </div >
   );
 }
 
