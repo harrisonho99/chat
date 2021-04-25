3f8a97d61e0e59fa458798e2b7f875a98151ee10
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-04-25 17:55:45 +0700

    add privateRequest

--- client/src/helper/request/request.js
+++ client/src/helper/request/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const publicRequest = axios.create({
   baseURL: 'http://localhost:4000/public/',
 });
@@ -6,5 +7,15 @@ const publicRequestMobile = axios.create({
   baseURL: 'http://192.168.30.107:4000/public/',
 });
 
+const privateRequest = (token) => {
+  return axios.create({
+    baseURL: "http://localhost:4000",
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    },
+  })
+}
+
 publicRequest.defaults.timeout = 10000;
-export { publicRequest, publicRequestMobile };
+export { publicRequest, publicRequestMobile, privateRequest };
